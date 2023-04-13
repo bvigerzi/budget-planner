dee77a9f27c7d40f28d88015334428ffb10d3419
bvigerzi <48936239+bvigerzi@users.noreply.github.com>
2023-04-13 22:23:42 +1000

    start gpt logic

--- gpt_categoriser.py
+++ gpt_categoriser.py
@@ -1,8 +1,24 @@
 import csv
+import math
 
-from parser import find_latest_valid_budget
+import tiktoken
+from more_itertools import divide
+from budget_parser import find_latest_valid_budget
+
+MAX_TOKENS: int = 2048
+MAX_INPUT_TOKENS: int = int(MAX_TOKENS / 2)
+
+# cl100k_base	gpt-4, gpt-3.5-turbo, text-embedding-ada-002
+# p50k_base	Codex models, text-davinci-002, text-davinci-003
+# r50k_base (or gpt2)	GPT-3 models like davinci
+#
+# source: https://github.com/openai/openai-cookbook/blob/main/examples/How_to_count_tokens_with_tiktoken.ipynb
+
+token_encoder = tiktoken.get_encoding("p50k_base")  # TODO: make this customisable so we can switch models easily
 
 
+# Assumes budget has the following structure:
+# Category, Sub-Category, Budget, Ignore
 def gpt_friendly_budget(monthly_budgets: list[str], statement: str) -> str:
     budget = find_latest_valid_budget(monthly_budgets, statement)
     output = ""
@@ -26,6 +42,8 @@ def gpt_friendly_budget(monthly_budgets: list[str], statement: str) -> str:
     return output
 
 
+# Assumes statement has the following structure:
+# Date, Description, Debit, Credit
 def gpt_friendly_statement(statement: str) -> str:
     output = ""
     with open(statement) as file:
@@ -36,3 +54,35 @@ def gpt_friendly_statement(statement: str) -> str:
             output = output + ",".join(row) + "\n"
     return output
 
+
+def pre_prompt() -> str:
+    return """
+        categorise this statement
+    """  # TODO: improve this
+
+
+def categorise_statement(gpt_budget: str, gpt_statement: str) -> None:
+    pre_prompt_text = pre_prompt()
+    budget_tokens = token_encoder.encode(gpt_budget)
+    statement_tokens = token_encoder.encode(gpt_statement)
+    pre_prompt_tokens = token_encoder.encode(pre_prompt_text)
+    budget_tokens_count = len(budget_tokens)
+    statement_tokens_count = len(statement_tokens)
+    pre_prompt_tokens_count = len(pre_prompt_tokens)
+    tokens_for_statement = MAX_INPUT_TOKENS - pre_prompt_tokens_count - budget_tokens_count
+    split_statement_by = math.ceil(statement_tokens_count / tokens_for_statement)
+    print("divide statement by {}".format(split_statement_by))
+    statement_split = list(map(lambda iterator: ''.join(iterator), list(divide(split_statement_by, gpt_statement))))
+    print(statement_split)
+    # Requirements:
+    # 1. need the header for each sub_statement -> extract it from gpt_statement and prepend on each iteration
+    # 2. this will change the calculations slightly
+    # 3. also need to re-think calculations because we should split exactly on newlines rather than by tokens
+    # 4. this is necessary because we cannot have a partial row sent to GPT
+
+    # split gpt_statement by split_statement_by and iterate over the substrings
+    # for each substring_statement:
+    # prompt = pre_prompt_text + gpt_budget + substring_statement
+    # query openai API completions, parse response
+    # hold onto response, it will form the partial categorised transactions
+    return
--- report_generator.py
+++ report_generator.py
@@ -3,27 +3,46 @@ import glob
 
 from calculator import compute_carry, compute_remainder, compute_remaining_spend, \
     compute_next_month_available_budget
-from parser import parse_latest_valid_budget, parse_monthly_statement, parse_monthly_statement_date
+from budget_parser import parse_latest_valid_budget, parse_monthly_statement, parse_monthly_statement_date
 from renderer import render_csv, render_html
-from gpt_categoriser import gpt_friendly_budget
+from gpt_categoriser import gpt_friendly_budget, gpt_friendly_statement, categorise_statement
 
 if __name__ == "__main__":
-    monthly_budget_csv_pattern: str = "./monthly_budget[0-9]*.csv"
-    expenses_statement_csv_pattern: str = "./SpendAccount[a-zA-Z0-9-]*[0-9]*-[0-9]*.csv"
-    monthly_budgets: list[str] = glob.glob(monthly_budget_csv_pattern)
-    monthly_expenses: list[str] = glob.glob(expenses_statement_csv_pattern)
+    monthly_budget_csv_pattern: str = "monthly_budget[0-9]*.csv"
+    expenses_statement_csv_pattern: str = "SpendAccount[a-zA-Z0-9-]*[0-9]*-[0-9]*.csv"
+    monthly_budget_dir = "./"
+    expenses_statement_pre_categorise_dir = "./pre-categorise/"
+    expenses_statement_post_categorise_dir = "./post-categorise/"
+
+    monthly_budgets: list[str] = glob.glob(monthly_budget_dir + monthly_budget_csv_pattern)
+
+    uncategorised_monthly_expenses: list[str] = glob.glob(
+        "{0}{1}".format(expenses_statement_pre_categorise_dir, expenses_statement_csv_pattern))
+    categorised_monthly_expenses: list[str] = glob.glob(
+        "{0}{1}".format(expenses_statement_post_categorise_dir, expenses_statement_csv_pattern))
+
     carry = None
     remainder = None
 
-    for statement in monthly_expenses:
+    print(uncategorised_monthly_expenses)
+
+    for statement in uncategorised_monthly_expenses:
         parsed_budget = parse_latest_valid_budget(monthly_budgets, statement)
-        print(gpt_friendly_budget(monthly_budgets, statement))
+        gpt_budget = gpt_friendly_budget(monthly_budgets, statement)
+        gpt_statement = gpt_friendly_statement(statement)
+        categorise_statement(gpt_budget, gpt_statement)
+
+    for statement in categorised_monthly_expenses:
+        # Parse budget and expenses step
+        parsed_budget = parse_latest_valid_budget(monthly_budgets, statement)
         parsed_statement = parse_monthly_statement(statement)
         statement_date = parse_monthly_statement_date(statement)
+        # Compute spend step
         carry = compute_carry(remainder, parsed_budget)
         remainder = compute_remainder(carry, parsed_budget, parsed_statement)
         remaining_spend = compute_remaining_spend(carry, parsed_budget)
         next_month_available = compute_next_month_available_budget(remainder, parsed_budget)
+        # Render report step
         render_csv(parsed_budget, parsed_statement, carry, remainder, remaining_spend, next_month_available,
                    statement_date)
         render_html(parsed_budget, parsed_statement, carry, remainder, remaining_spend, next_month_available,
